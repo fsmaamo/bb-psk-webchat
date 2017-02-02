24504298f32ff45a2181083d60ba5c60734dd2e5
Fredrik <smaamo89@gmail.com>
2017-02-02 15:42:01 +0100

    no message

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 var connect = require('connect');
 var serveStatic = require('serve-static');
-connect().use(serveStatic(__dirname + '/staging')).listen(8080, function(){
+connect().use(serveStatic(__dirname + '/publish')).listen(8080, function(){
     console.log('Server running on 8080...');
 });
